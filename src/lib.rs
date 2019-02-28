d9a47997e4be367da554b82cdf41ffc194f86528
David Kellum <dek-oss@gravitext.com>
2019-02-28 06:50:25 -0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,171 @@
+// Copyright Ⓒ 2019 David Kellum
+//
+// Documentation here is derivative work from:
+//
+// * https://github.com/rust-lang-nursery/log/pull/316 (same author)
+// * https://github.com/rust-lang-nursery/log
+//
+// Offered under the same Apache 2.0 or MIT licenses and is:
+// Copyright Ⓒ 2015 The Rust Project Developers
+
+//! Extension macros for output to the rust _log_ crate.
+//!
+//! > <em>“Why write an RFC, when you can just write code?”</em>
+//! > — 老子 (“Old Master”), 557 BCE
+//!
+//! This unenlightened wanderer (游侠) wrote a _log_ [PR], [RFC 317], and
+//! _only then_ published this standalone crate, in the hopes of gaining
+//! insights through use or abuse in the wild.
+//!
+//! Libraries and applications can use the _tao-log_ macros for all log
+//! output needs. This includes re-exported _log_ crate _formatted logging_
+//! macros, and a set of new “_-v_” suffix macros (e.g. `debugv!`) that
+//! provide _inline expression and value_ logging. Both sets of macros are
+//! given a new overview below, with examples using arbitrary log
+//! levels.
+//!
+//! If the new macros are eventually accepted to the _log_ crate in compatible
+//! form, this crate will be re-released with those re-exports, and possible
+//! deprecated in the future.
+//!
+//! Using the rust 2018 edition, all exported macros can be imported like a
+//! prelude:
+//!
+//! ```rust
+//! use tao_log::*;
+//! ```
+//! Or if desired, imported on a per macro basis:
+//!
+//! ```rust
+//! use tao_log::{debugv, debug, error, warn};
+//! ```
+//!
+//! ### Formatted logging
+//!
+//! The original macros for each supported level, from highest to lowest
+//! priority (or lowest to highest verbosity) include: `error!`, `warn!`,
+//! `info!`, `debug!` and `trace!`. These are passed a literal format string,
+//! supporting the same syntax as `println!`, via [`std::fmt`]:
+//!
+//! ```rust
+//! use tao_log::*;
+//!
+//! info!("Landing gear retracted");
+//! let altitude = 3000;
+//! let target = 10500;
+//! debug!("Altitude target: {}, current: {}", target, altitude);
+//! ```
+//!
+//! If for example, the configured maximum logging level is `Info`, then the
+//! above `debug!` statement does not log, and the cost of formatting the
+//! message string is avoided.
+//!
+//! ### Testing for output
+//!
+//! The [`log_enabled!`](macro.log_enabled.html) macro may be used to
+//! explicitly test if logging is enabled, and may be useful to avoid
+//! expensive computations used only for logging.
+//!
+//! ```rust
+//! # struct Foo;
+//! # impl Foo {
+//! #     fn volume(&self) -> f32 { 0.1 }
+//! #     fn mass(&self) -> f32 { 0.2 }
+//! # }
+//! # fn analyze(a: u32) -> Foo { Foo }
+//! use log::Level::Debug;
+//! use tao_log::*;
+//!
+//! # fn main() {
+//! # let asteroid = 1;
+//! if log_enabled!(Debug) {
+//!     let e = analyze(asteroid); // expensive!
+//!     debug!("Asteroid volume: {}, mass: {}", e.volume(), e.mass());
+//! }
+//! # }
+//! ```
+//!
+//! ### Inline expression and value logging
+//!
+//! The _-v_ macros support inline expression and value logging, as a superset
+//! of the [`std::dbg!`] macro, for use with the logging system. A _single_
+//! expression argument is evaluated exactly once, regardless of if the
+//! logging level is enabled, and its value is returned from the macro. Given
+//! this code as a starting point:
+//!
+//! ```rust
+//! use std::time::{Duration, Instant};
+//! # let deadline = Instant::now() + Duration::new(120, 0);
+//!
+//! let remaining = deadline - Instant::now();
+//! ```
+//!
+//! A _-v_ macro may be inserted inline around any expression or
+//! sub-expression:
+//!
+//! ```rust
+//! use std::time::{Duration, Instant};
+//! use tao_log::*;
+//! # let deadline = Instant::now() + Duration::new(0, 950_000);
+//!
+//! let remaining = debugv!(deadline - Instant::now());
+//! //                ^-- debug log: deadline - Instant::now() → 950µs
+//!
+//! debugv!(remaining); // alternative
+//! // ^--- debug log: remaining → 950µs
+//! ```
+//!
+//! Note that the value of the expression is moved and then returned. If the
+//! type does not implement `Copy`, ownership may be retained by borrowing by
+//! reference, e.g. `debugv!(&expr)`.
+//!
+//! The _default_ format string for the _-v_ macros is `"{} → {:?}"`, where
+//! the `stringify!`-ed expression and resulting value are passed, in that
+//! order.  If the log record is not filtered out, the `Debug` implementation
+//! for the type of the given expression value is used (`{:?}`).  A custom
+//! format string can _also_ be passed to the _-v_ macros, for more flexible
+//! output:
+//!
+//! ```rust
+//! use tao_log::*;
+//!
+//! let i = 32;
+//! infov!("{} → {}", i);            // use `Display` instead of `Debug`
+//! infov!("{} → {:#x}", i);         // hexadecimal format value
+//! infov!("{} → {:#?}", i);         // use pretty, multi-line format
+//! infov!("index {1:5?} ({0})", i); // prefix, value first with padding
+//! ```
+//!
+//! ### Specifying the logging target
+//!
+//! For _all_ logging macros, the _target_ defaults to the module path of the
+//! current location of use, but may be overridden with the `target:` marker
+//! and string literal as the first argument:
+//!
+//! ```rust
+//! # fn stats() -> i32 { 33 }
+//! # fn main() {
+//! use tao_log::*;
+//! use log::Level::Info;
+//!
+//! let i = 33;
+//! let j = warnv!(target: "maths", (i-1) / 2);
+//! # assert_eq!(j, 16);
+//!
+//! if log_enabled!(target: "special", Info) {
+//!     info!(target: "special", "{}", stats());
+//! }
+//! # }
+//! ```
+//!
+//! [PR]: https://github.com/rust-lang-nursery/log/pull/316
+//! [RFC 317]: https://github.com/rust-lang-nursery/log/pull/317
+//! [`std::fmt`]: https://doc.rust-lang.org/stable/std/fmt/index.html
+//! [`std::dbg!`]: https://doc.rust-lang.org/std/macro.dbg.html
+
+#![doc(html_logo_url = "https://upload.wikimedia.org/wikipedia/commons/7/7c/Yin_and_Yang.svg")]
+
+pub use ::log;
+pub use log::{debug, error, info, log, log_enabled, trace, warn};
+
+#[macro_use] mod macros;
---
+++ src/macros.rs
@@ -0,0 +1,134 @@
+// Copyright Ⓒ 2019 David Kellum
+//
+// Beyond the macro public re-exports found here, the _-v_ macros were
+// co-developed with https://github.com/rust-lang-nursery/log/pull/316 (by the
+// same author) and some other support macros are adopted from log 0.4.6.
+//
+// All original work is offered under the same Apache 2.0 or MIT licenses and
+// is:
+// Copyright Ⓒ 2015 The Rust Project Developers
+
+/// Log an expression and its value at any specified level.
+///
+/// Logs with the optional or default (module path of use) target, specified
+/// `Level`, optional or default (`"{} → {:?}"`) format string, and a single
+/// expression and value, which is returned.  This is normally only used
+/// through the _-v_ macros like `debugv!` or `tracev!`.
+///
+/// Note that the value is moved and then returned. If the type does not
+/// implement `Copy`, ownership may be retained by borrowing by reference
+/// e.g. `debugv!(&expr)`.
+///
+/// # Examples
+///
+/// ```rust
+/// use log::Level;
+/// use tao_log::*;
+///
+/// #[derive(Debug)]
+/// struct Point { x: f32, y: f32 }
+///
+/// fn circle(center: &Point, radius: f32) { /*...*/ }
+///
+/// # fn main() {
+/// let center = Point { x: 3.234, y: -1.223 };
+///
+/// circle(logv!(Level::Trace, &center), 7.3);
+/// //     ^-- trace level message: "&center → Point { x: 3.234, y: -1.223 }"
+/// circle(tracev!(&center), 8.0);
+/// //     ^-- trace level message: "&center → Point { x: 3.234, y: -1.223 }"
+/// # }
+/// ```
+#[macro_export(local_inner_macros)]
+macro_rules! logv {
+    (target: $target:expr, $lvl:expr, $($arg:tt)+) => (
+        __logv!($lvl, target: $target, $($arg)+)
+    );
+    ($lvl:expr, $($arg:tt)+) => (
+        __logv!($lvl, target: __log_module_path!(), $($arg)+)
+    );
+}
+
+/// Log an expression at the error level, returning the value.
+#[macro_export(local_inner_macros)]
+macro_rules! errorv {
+    ($($arg:tt)+) => (__logv!(log::Level::Error, $($arg)+))
+}
+
+/// Log an expression at the warn level, returning the value.
+#[macro_export(local_inner_macros)]
+macro_rules! warnv {
+    ($($arg:tt)+) => (__logv!(log::Level::Warn, $($arg)+))
+}
+
+/// Log an expression at the info level, returning the value.
+#[macro_export(local_inner_macros)]
+macro_rules! infov {
+    ($($arg:tt)+) => (__logv!(log::Level::Info, $($arg)+))
+}
+
+/// Log an expression at the debug level, returning the value.
+#[macro_export(local_inner_macros)]
+macro_rules! debugv {
+    ($($arg:tt)+) => (__logv!(log::Level::Debug, $($arg)+))
+}
+
+/// Log an expression at the trace level, returning the value.
+#[macro_export(local_inner_macros)]
+macro_rules! tracev {
+    ($($arg:tt)+) => (__logv!(log::Level::Trace, $($arg)+))
+}
+
+// Helper macro for the -v macros, handling the permutations of optional
+// parameters. Note: The required level paremeter is first here for
+// convenience of internal use with variable-args.
+#[doc(hidden)]
+#[macro_export(local_inner_macros)]
+macro_rules! __logv {
+    ($lvl:expr, target: $tgt:expr, $fmt:expr, $val:expr) => (
+        __logv_eval!($tgt, $lvl, $fmt, $val)
+    );
+    ($lvl:expr, target: $tgt:expr, $val:expr) => (
+        __logv_eval!($tgt, $lvl, "{} → {:?}", $val)
+    );
+    ($lvl:expr, $fmt:expr, $val:expr) => (
+        __logv_eval!(__log_module_path!(), $lvl, $fmt, $val)
+    );
+    ($lvl:expr, $val:expr) => (
+        __logv_eval!(__log_module_path!(), $lvl, "{} → {:?}", $val)
+    );
+}
+
+// Inner helper macro for __logv. Evaluates expression exactly once, moves
+// value and returns it.
+#[doc(hidden)]
+#[macro_export(local_inner_macros)]
+macro_rules! __logv_eval {
+    ($tgt:expr, $lvl:expr, $fmt:expr, $exp:expr) => (
+        match $exp {
+            vt => {
+                log!(target: $tgt, $lvl, $fmt, __log_stringify!($exp), &vt);
+                vt
+            }
+        }
+    )
+}
+
+// As a workaround for rustc < 1.30, provide thse local wrappers for the above
+// macros with local_inner_macros.
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __log_module_path {
+    () => {
+        module_path!()
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __log_stringify {
+    ($($args:tt)*) => {
+        stringify!($($args)*)
+    };
+}
