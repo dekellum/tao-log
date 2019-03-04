c6279b18b4237da22aa2dbf4381fe95808fd5828
David Kellum <dek-oss@gravitext.com>
2019-03-04 10:32:39 -0800

    compiler error tests, certain nightlies only

---
+++ test_compile_errors/tests/compile-fail/v.rs
@@ -0,0 +1,14 @@
+//! Tests of *v (inline expression value) log macro compile failures
+
+extern crate tao_log;
+use tao_log::*;
+
+fn heap_string_as_prefix() {
+    let ctx: String = "contextual prefix".to_owned();
+    warnv!(&ctx, 1); //~ ERROR expected a literal
+}
+
+fn extra_placeholder_in_prefix() {
+    let i = 4;
+    warnv!("bad output verbatim {:?}", i); //~ ERROR 3 positional arguments in format string, but there are 2 arguments
+}
