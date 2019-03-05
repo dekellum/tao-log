cc27fad8a1df2dbe47ef3bb23f4e2a46545f625b
David Kellum <dek-oss@gravitext.com>
2019-03-04 16:59:57 -0800

    Drop use of local_inner_macros for $crate on 1.30+

--- src/macros.rs
+++ src/macros.rs
@@ -1,8 +1,7 @@
 // Copyright Ⓒ 2019 David Kellum
 //
 // These _-v_ macros were co-developed with
-// https://github.com/rust-lang-nursery/log/pull/316 (by the same author) and
-// some other support macros are adopted from log 0.4.6.
+// https://github.com/rust-lang-nursery/log/pull/316 (by the same author)
 //
 // All original _log_ source is offered under the same Apache 2.0 or MIT
 // licenses, and is:
@@ -40,109 +39,84 @@
 /// //     ^-- trace level message: "&center → Point { x: 3.234, y: -1.223 }"
 /// # }
 /// ```
-#[macro_export(local_inner_macros)]
+#[macro_export]
 macro_rules! logv {
     (target: $target:expr, $lvl:expr, $($arg:tt)+) => (
-        __logv!($lvl, target: $target, $($arg)+)
+        $crate::__tao_logv!($lvl, target: $target, $($arg)+)
     );
     ($lvl:expr, $($arg:tt)+) => (
-        __logv!($lvl, target: __tao_target!(), $($arg)+)
+        $crate::__tao_logv!($lvl, target: module_path!(), $($arg)+)
     );
 }
 
 /// Log an expression at the error level, returning its value.
-#[macro_export(local_inner_macros)]
+#[macro_export]
 macro_rules! errorv {
-    ($($arg:tt)+) => (__logv!($crate::log::Level::Error, $($arg)+))
+    ($($arg:tt)+) => ($crate::__tao_logv!($crate::log::Level::Error, $($arg)+))
 }
 
 /// Log an expression at the warn level, returning its value.
-#[macro_export(local_inner_macros)]
+#[macro_export]
 macro_rules! warnv {
-    ($($arg:tt)+) => (__logv!($crate::log::Level::Warn, $($arg)+))
+    ($($arg:tt)+) => ($crate::__tao_logv!($crate::log::Level::Warn, $($arg)+))
 }
 
 /// Log an expression at the info level, returning its value.
-#[macro_export(local_inner_macros)]
+#[macro_export]
 macro_rules! infov {
-    ($($arg:tt)+) => (__logv!($crate::log::Level::Info, $($arg)+))
+    ($($arg:tt)+) => ($crate::__tao_logv!($crate::log::Level::Info, $($arg)+))
 }
 
 /// Log an expression at the debug level, returning its value.
-#[macro_export(local_inner_macros)]
+#[macro_export]
 macro_rules! debugv {
-    ($($arg:tt)+) => (__logv!($crate::log::Level::Debug, $($arg)+))
+    ($($arg:tt)+) => ($crate::__tao_logv!($crate::log::Level::Debug, $($arg)+))
 }
 
 /// Log an expression at the trace level, returning its value.
-#[macro_export(local_inner_macros)]
+#[macro_export]
 macro_rules! tracev {
-    ($($arg:tt)+) => (__logv!($crate::log::Level::Trace, $($arg)+))
+    ($($arg:tt)+) => ($crate::__tao_logv!($crate::log::Level::Trace, $($arg)+))
 }
 
 // Helper macro for the -v macros, handling the permutations of optional
 // parameters. Note: The required level parameter is first here for
 // convenience of internal use with variable-args.
 #[doc(hidden)]
-#[macro_export(local_inner_macros)]
-macro_rules! __logv {
+#[macro_export]
+macro_rules! __tao_logv {
     ($lvl:expr, target: $tgt:expr, $pre:expr, $vfmt:expr, $exp:expr) => (
-        __logv_eval!($tgt, $lvl, __tao_concat!($pre, " {} → ", $vfmt), $exp)
+        $crate::__tao_v_eval!($tgt, $lvl, concat!($pre, " {} → ", $vfmt), $exp)
     );
     ($lvl:expr, target: $tgt:expr, $pre:expr, $exp:expr) => (
-        __logv_eval!($tgt, $lvl, __tao_concat!($pre, " {} → {:?}"), $exp)
+        $crate::__tao_v_eval!($tgt, $lvl, concat!($pre, " {} → {:?}"), $exp)
     );
     ($lvl:expr, target: $tgt:expr, $exp:expr) => (
-        __logv_eval!($tgt, $lvl, "{} → {:?}", $exp)
+        $crate::__tao_v_eval!($tgt, $lvl, "{} → {:?}", $exp)
     );
     ($lvl:expr, $pre:expr, $vfmt:expr, $exp:expr) => (
-        __logv_eval!(__tao_target!(), $lvl, __tao_concat!($pre, " {} → ", $vfmt), $exp)
+        $crate::__tao_v_eval!(module_path!(), $lvl, concat!($pre, " {} → ", $vfmt), $exp)
     );
     ($lvl:expr, $pre:expr, $exp:expr) => (
-        __logv_eval!(__tao_target!(), $lvl, __tao_concat!($pre, " {} → {:?}"), $exp)
+        $crate::__tao_v_eval!(module_path!(), $lvl, concat!($pre, " {} → {:?}"), $exp)
     );
     ($lvl:expr, $exp:expr) => (
-        __logv_eval!(__tao_target!(), $lvl, "{} → {:?}", $exp)
+        $crate::__tao_v_eval!(module_path!(), $lvl, "{} → {:?}", $exp)
     );
 }
 
-// Inner helper macro for __logv. Evaluates expression exactly once, moves
+// Inner helper macro for __tao_logv. Evaluates expression exactly once, moves
 // value and returns it.
 #[doc(hidden)]
-#[macro_export(local_inner_macros)]
-macro_rules! __logv_eval {
+#[macro_export]
+macro_rules! __tao_v_eval {
     ($tgt:expr, $lvl:expr, $fmt:expr, $exp:expr) => (
         match $exp {
             vt => {
-                $crate::log!(
-                    target: $tgt, $lvl, $fmt,
-                    __tao_stringify!($exp), &vt
-                );
+                $crate::log!(target: $tgt, $lvl, $fmt, stringify!($exp), &vt);
                 vt
             }
         }
     )
 }
 
-// As a workaround for rustc < 1.30, provide these local wrappers for the
-// above macros with local_inner_macros. Note our MSRV is currently higher,
-// but we preserve this approach until it is certain we don't want to
-// backport.
-
-#[doc(hidden)]
-#[macro_export]
-macro_rules! __tao_target {
-    () => (module_path!())
-}
-
-#[doc(hidden)]
-#[macro_export]
-macro_rules! __tao_stringify {
-    ($($args:tt)*) => (stringify!($($args)*))
-}
-
-#[doc(hidden)]
-#[macro_export]
-macro_rules! __tao_concat {
-    ($($args:tt)*) => (concat!($($args)*))
-}
